8ab1ebd37528fb0f761cf70606c4ea52316bd3b5
bezumnui <Bezumnui.mistikgt@gmail.com>
2022-07-07 14:02:57 +0400

    setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='DonationAlertsHandler',
+      version='1.0',
+      description='Donation Alerts Handler',
+      author='bezumnui',
+      author_email='bezumnui.mistikgt@gmail.com',
+      url='https://github.com/TGeniusFamily/DonationAlertsHandler',
+      packages=['aiohttp'],
+     )
